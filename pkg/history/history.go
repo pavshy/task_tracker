0c133bd857d7ad54ae308ffe5d09c8989e5839fe
shishkinpi22@gmail.com <zTLHvcpIZPbs7SZmWmEvfQvcLM0Sxe>
2021-09-15 20:45:57 +0300

    Parsing history

--- pkg/history/history.go
+++ pkg/history/history.go
@@ -2,6 +2,7 @@ package history
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"time"
@@ -15,12 +16,29 @@ func inFolder(fileName string) string {
 	return path.Join(SavingPath, fileName)
 }
 
+func Load() (string, error) {
+	err := os.MkdirAll(SavingPath, 0777)
+	if err != nil {
+		return "", fmt.Errorf("cannot create dirs for history: %w", err)
+	}
+	currentDate := time.Now().UTC().Format("2006-01-02")
+	f, err := os.Open(inFolder(currentDate))
+	if err != nil {
+		return "", fmt.Errorf("cannot create file for history: %w", err)
+	}
+	defer f.Close()
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", fmt.Errorf("cannot write report: %w", err)
+	}
+	return string(content), nil
+}
+
 func Save(report string) error {
 	err := os.MkdirAll(SavingPath, 0777)
 	if err != nil {
 		return fmt.Errorf("cannot create dirs for history: %w", err)
 	}
-	fmt.Println(time.Now().UTC())
 	currentDate := time.Now().UTC().Format("2006-01-02")
 	f, err := os.Create(inFolder(currentDate))
 	if err != nil {
--- pkg/tasks/tasks.go
+++ pkg/tasks/tasks.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
@@ -23,6 +25,12 @@ type Task struct {
 }
 
 func Listen() error {
+	histContent, err := history.Load()
+	if err != nil {
+		fmt.Printf("error loading history: %v\n", err)
+	}
+	TodayTasks = ParseHistory(histContent)
+
 	fmt.Println("Enter time and task")
 	for {
 		in := bufio.NewReader(os.Stdin)
@@ -35,6 +43,13 @@ func Listen() error {
 		switch ln {
 		case "q", "quit", "exit":
 			return fmt.Errorf("exiting")
+		case "":
+			cls()
+			report, err := FormReport(TodayTasks)
+			if err != nil {
+				return fmt.Errorf("form reprot error: %w", err)
+			}
+			fmt.Println(report)
 		default:
 			spltLn := strings.SplitN(ln, " ", 2)
 			if len(spltLn) != 2 {
@@ -68,6 +83,7 @@ func Listen() error {
 			if err != nil {
 				return fmt.Errorf("error saving report: %w", err)
 			}
+			cls()
 			fmt.Printf(report)
 		}
 	}
@@ -78,6 +94,12 @@ func FormReport(tasks Tasks) (string, error) {
 	_, err := fmt.Fprintf(buf, "Today:\n")
 	if err != nil {
 		return "", fmt.Errorf("cannot write today's history to file: %w", err)
+	}
+	if len(tasks) == 0 {
+		_, err := fmt.Fprintf(buf, "nothing today\n")
+		if err != nil {
+			return "", fmt.Errorf("cannot write today's history to file: %w", err)
+		}
 	}
 	for _, task := range tasks {
 		var timeStr string
@@ -94,4 +116,52 @@ func FormReport(tasks Tasks) (string, error) {
 		}
 	}
 	return buf.String(), nil
+}
+
+func ParseHistory(hist string) Tasks {
+	var tasks = make(Tasks)
+linesLoop:
+	for _, line := range strings.Split(hist, "\n") {
+		line = strings.TrimSpace(line)
+		split := strings.SplitN(line, " ", 2)
+		if len(split) != 2 {
+			continue
+		}
+		spentStr := split[0]
+		var hrs, mins int
+		spentSplit := strings.Split(spentStr, "ч")
+		switch len(spentSplit) {
+		case 2:
+			var err error
+			hrs, err = strconv.Atoi(strings.TrimSpace(spentSplit[0]))
+			if err != nil {
+				continue
+			}
+			mins, err = strconv.Atoi(strings.Trim(spentSplit[1], "\n\t м"))
+			if err != nil {
+				continue
+			}
+		case 1:
+			var err error
+			mins, err = strconv.Atoi(strings.Trim(spentSplit[0], "\n\t м"))
+			if err != nil {
+				continue
+			}
+		default:
+			continue linesLoop
+		}
+		spent := time.Duration(hrs)*time.Hour + time.Duration(mins)*time.Minute
+		taskName := split[1]
+		tasks[taskName] = Task{
+			Name:  taskName,
+			Spent: spent,
+		}
+	}
+	return tasks
+}
+
+func cls() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	_ = cmd.Run()
 }
